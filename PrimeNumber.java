ebc906b44aa4824f39d1fb093f5bd05df9431eb6
Sanchita Rathod <Sanchu.03.07@gmail.com>
2026-06-21 20:03:40 +0530

    removed class files and added gitignore

---
+++ PrimeNumber.java
@@ -0,0 +1,18 @@
+public class PrimeNumber {
+    public static void main(String[] args){
+        System.out.println("Printing the prime numbers from 1 to 100");
+        for(int i = 1 ; i < 101 ; i++){
+            boolean isPrime=true;
+            
+            for(int j=2 ; j < i ; j++){
+                if(i%j==0){
+                isPrime=false;
+                break;
+                }
+            }
+            if(isPrime==true && i>1){
+                System.out.print(i+"    ");
+            }
+        }
+    }
+}
