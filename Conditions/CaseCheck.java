5147997de719905854b038c97a64afc21a25bade
Sanchita Rathod <Sanchu.03.07@gmail.com>
2026-06-28 20:12:17 +0530

    Check case!!

---
+++ Conditions/CaseCheck.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CaseCheck {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Character to check!!");
+        char ch =sc.next().trim().charAt(0);
+
+        if(ch>='a' && ch<='z'){
+            System.out.println("LowerCase");
+        }
+        else{
+            System.out.println("UpperCase");
+        }
+    }    
+}
