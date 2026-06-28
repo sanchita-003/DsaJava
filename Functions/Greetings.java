ff7f62c265aaf00633fa75b691c29d139e6c2c5f
Sanchita Rathod <Sanchu.03.07@gmail.com>
2026-06-28 21:02:16 +0530

    Used function to greet

---
+++ Functions/Greetings.java
@@ -0,0 +1,9 @@
+public class Greetings {
+    static void greet(){
+        System.out.println("Hello this is Sanchita!!");
+    }
+    public static void main(String[] args){
+        greet();
+    }
+    
+}
