72d294dc9a9e9a3d1ca6e8df3a395147014936b3
Sanchita Rathod <Sanchu.03.07@gmail.com>
2026-06-29 16:40:32 +0530

    Passed the name to function

---
+++ Functions/Pass.java
@@ -0,0 +1,9 @@
+public class Pass {
+    public static void main(String[] args) {
+        String name = "campus";
+        print(name);
+    }
+    static void print(String nam){
+        System.out.println(nam);
+    }
+}
