3943fe28dcfae31d340261d5f33862666614a909
Sanchita Rathod <Sanchu.03.07@gmail.com>
2026-07-28 22:44:40 +0530

    Comparison

---
+++ strings/Comparison.java
@@ -0,0 +1,9 @@
+public class Comparison {
+    public static void main(String[] args) {
+        String name = "Kunal";
+        String naam = "Kunal";
+        
+        System.out.println(name==naam);
+        System.out.println(name.equals(naam));
+    }
+}
