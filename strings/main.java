de6a2c67aca16a37588e9799b057d424358e71bb
Sanchita Rathod <Sanchu.03.07@gmail.com>
2026-07-28 22:39:22 +0530

    String

---
+++ strings/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        String a = "Sanchita";
+        System.out.println(a);
+        a = "Rathod";
+        System.out.println(a);
+    }
+}
