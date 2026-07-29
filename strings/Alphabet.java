6fd8fc3cfe3667b6f87fc28a7274a3f76324a4b3
Sanchita Rathod <Sanchu.03.07@gmail.com>
2026-07-29 22:31:10 +0530

    learned ASCII value

---
+++ strings/Alphabet.java
@@ -0,0 +1,10 @@
+public class Alphabet {
+    public static void main(String[] args) {
+        String s = "";
+        for(int i = 0 ; i < 26 ; i++){
+            char ch = (char)('a'+i);
+            s = s+ch;
+        }
+        System.out.println(s);
+    }
+}
