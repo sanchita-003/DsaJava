0446490580a4491a3b887bd54eb28674264914f1
Sanchita Rathod <Sanchu.03.07@gmail.com>
2026-07-29 22:42:35 +0530

    learned to use the placeholder properly

---
+++ strings/Printing.java
@@ -0,0 +1,14 @@
+public class Printing {
+    public static void main(String[] args) {
+        float a = 431.21124567f;
+        System.out.printf(" %.2f",a);
+        //print floating digit till 2 digit after decimal
+
+        System.out.printf("\n%.3f",a);
+        //print floating digit till 3 digit after the decimal
+
+        System.out.printf("\nHello my name is %s and i am %s " , "sanchita" , "Student");
+        //%s is the placeholder , we can replace string in order here
+
+    }
+}
