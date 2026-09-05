e2d0b5cd7d3cd87fdda0c10fd5d4d4364bc57799
Sanchita Rathod <Sanchu.03.07@gmail.com>
2026-09-05 15:16:16 +0530

    Printed pattern using recurssion

--- recurrsion/Pattern/main.java
+++ recurrsion/Pattern/main.java
@@ -3,31 +3,30 @@ public class main {
         triangle2(5, 0);
         
     }
-    static void triangle(int r , int col){
+    static void triangle(int r , int c){
         if(r == 0){
             return;
         }
-        if(r > col){
-            System.out.print("*");
-            triangle(r, col+1);
+        if(r > c){
+            System.out.print("* ");
+            triangle(r, c+1);
         }
         else{
             System.out.println(" ");
             triangle(r-1, 0);
         }
     }
-
-    static void triangle2(int r  , int c){
+    static void triangle2(int r , int c){
         if(r == 0){
             return;
         }
         if(r > c){
             triangle2(r, c+1);
-            System.out.print("*");
+            System.out.print("* ");
         }
         else{
             triangle2(r-1, 0);
-            System.out.println("");
+            System.out.println(" ");
         }
     }
 }
