db2f41d23a59204b496af2d82a34320f46dc9916
Sanchita Rathod <Sanchu.03.07@gmail.com>
2026-09-24 21:13:24 +0530

    Ascii value

---
+++ recurrsion/String/Ascii.java
@@ -0,0 +1,11 @@
+public class Ascii {
+    public static void main(String[] args) {
+        char ch = 'a';
+        System.out.println(ch);
+        System.out.println((char)(ch + 0));
+        System.out.println(ch + 0);
+        System.out.println(ch + '0');
+        System.out.println(ch - 0);
+        
+    }
+}
